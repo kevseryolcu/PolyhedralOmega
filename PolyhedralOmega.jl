fc5ea0df2864f09a04b862b763abf6d44428e151
Kevser Yolcu <kyolcu@gtu.edu.tr>
2021-01-24 15:48:30 +0300

    fixed eliminate last coordinate and completed enumerate parallel piped

--- PolyhedralOmega.jl
+++ PolyhedralOmega.jl
@@ -1,5 +1,7 @@
 module PolyhedralOmega
 using LinearAlgebra
+using IterTools
+#using LazySet
 
 include("SymbolicCone.jl")
 include("./SmithNormalForm/src/SmithNormalForm.jl")
@@ -48,39 +50,42 @@ function elimLastCoordinate(C::SymbolicCone)
     # println("filter1: ", filter(x -> V[n, x] > 0, Vector(1:k)))
     # println("filter2: ", filter(x -> V[n, x] <= 0, Vector(1:k)))
     #println("qn: ", q[n])
-
-    if (q[n] < 0)
-        append!(II, filter(x -> V[n, x] > 0, Vector(1:k)))
+    Iplus = filter(x -> V[n, x] > 0, Vector(1:k))
+    Iminus = filter(x -> V[n, x] < 0, Vector(1:k))
+    if (q[n] < 0 || (q[n] == 0 && size(Iplus, 1) <= size(Iminus, 1) && size(Iplus, 1) >= 1) )
+        II = Iplus
     else
         res = [SymbolicCone(prim(V[1:n-1, :]), q[1:n-1], o[1:n-1])]
-        append!(II, filter(x -> V[n, x] < 0, Vector(1:k)))
+        II = Iminus
     end
     #Cprime if q[n] is zero
 
-
-    Id = Matrix(1I, size(V, 1), size(V, 2))
-    T = []
     for j in II
-        T = -1 * Id * V[n, j]
-        z = zeros(Int64, size(V, 1), size(V, 2))
-        z[j, :] = V[n, :]
-        T = T + z
-        T[j, j] = -1
+        L = []
+        for i in 1:k
+            if i == j
+                if q[n] >= 0
+                    push!(L, -1*V[:,j])
+                 else
+                    push!(L, V[:,j])
+                 end
+            else
+                push!(L, (q[n] >= 0 ? 1 : -1)*( (V[n,i] * V[:,j]) + (-V[n,j]*V[:,i]) ))
+            end
+        end
 
-        prim_res = prim(V)
-        new_V = T + prim_res
-        new_V = new_V[1:end.!=n, 1:end]
-
-        new_o = o[j] != 0 ? o[:, j] + (0,) + o[j+1, :] : deepcopy(o)
+        new_o = o[j] != false ? vcat(o[1:j-1], false, o[j+1:end]) : deepcopy(o)
+        new_V = prim(hcat(L...))
+        new_V = new_V[1:(size(new_V, 1)-1),:]
 
         new_q = map(+, q, (-q[n] / (V[n, j])) * V[:, j])
         new_q = new_q[1:n-1]
-
         new_cone = SymbolicCone(new_V, new_q, new_o)
 
         flip_res = flip(new_cone)
         push!(res, flip_res)
     end
+    #######
     return res
 end
 
@@ -105,7 +110,7 @@ function eliminateCoordinates(C::SymbolicCone, k::Int)
         for j = 1:length(res)
             eres = elimLastCoordinate(res[j])
             #println("eres: ", eres)
-            push!(res2, eres)
+            append!(res2, eres)
             #println("res2: ", res2)
         end
         res = res2
@@ -118,22 +123,22 @@ end
 function enumerateFundamentalParallelePiped(C::SymbolicCone)
     SMFRes = SmithNormalForm.smith(C.V)
     S = SmithNormalForm.diagm(SMFRes)
-    Uinv = SMFRes.S
-    Winv = SMFRes.T
+    Uinv = inv(SMFRes.S)
+    Winv = inv(SMFRes.T)
     dimension = size(C.V, 2) # num of rows
     ambientDimension = size(C.V, 1) # num of cols
-
-    println("S: ", S, "\nUinv: ", Uinv, "\nWinv: ", Winv)
-    println("size 1: ", size(S,1), "\nsize 2: ", size(S,2))
+    #println("C.V: ", C.V)
+    #println("S: ", S, "\nUinv: ", Uinv, "\nWinv: ", Winv)
+    #println("size 1: ", size(S,1), "\nsize 2: ", size(S,2))
 
     diagonals = Int64[]
     for i in 1:dimension
         if(i <= size(S,1) && i <= size(S,2))
-            println("i: ", i)
+            #println("i: ", i)
             push!(diagonals, S[i,i])
         end
     end
-    println("Diagonals: ", diagonals)
+    #println("Diagonals: ", diagonals)
 
     lastDiagonal = diagonals[end]
 
@@ -142,52 +147,102 @@ function enumerateFundamentalParallelePiped(C::SymbolicCone)
     for d in diagonals
         push!(primeDiagonals, Int64(lastDiagonal/d))
     end
-    println("Prime diagonals: ", primeDiagonals)
+    #println("Prime diagonals: ", primeDiagonals)
 
     # qhat = Uinv * q
     apex = C.q
-    println("q: ", apex, "\nV: ", C.V)
+    #println("q: ", apex, "\nV: ", C.V)
 
     qhat = Uinv*apex
-    println("qhat: ", qhat)
+    #println("qhat: ", qhat)
 
     # Wprime
     Wprime = [Winv[j,i]*primeDiagonals[i] for i = 1:dimension, j = 1:dimension] #Winv * primeDiagonals
-    println("wprime: ", Wprime)
+    #println("wprime: ", Wprime)
 
     # qtrans
     qtrans = [sum([-Wprime[j,i] * qhat[i] for i = 1:dimension]) for j = 1:dimension]
-    println("qtrans: ", qtrans)
+    #println("qtrans: ", qtrans)
 
     #qfrac
     qfrac = [qtrans[i] - floor.(Int, qtrans[i]) for i = 1:dimension]
-    println("qfrac: ", qfrac)
+    #println("qfrac: ", qfrac)
 
     #qint
     qint = [ floor.(Int, qi) for qi in qtrans ]
-    println("qint: ", qint)
+    #println("qint: ", qint)
 
     #qsummand
     qsummand = [Int64(qi) for qi in (lastDiagonal*apex + C.V*qfrac) ]
-    println("qsummand", qsummand)
+    #println("qsummand", qsummand)
 
     #openness
     openness = [ (qfrac[j] == 0 ? C.o[j] : 0) for j in 1:dimension]
-    println("openness: ", openness)
+    #println("openness: ", openness)
 
     #bigP
-    res1 = [[1:1:diagonals[i];] for i= 1:dimension]
-    println("res1: ", res1)
+    #res1 = [[1:1:diagonals[i];] for i= 1:dimension]
+    #println("res1: ", res1)
+
+    # CartesianProduct( *[xrange(s[i]) for i in 1:k] )
+    L = []
+    P = []
+    for v in IterTools.product([1:diagonals[i] for i in 1:dimension]...)
+        push!(P, v)
+        innerRes = []
+        j = 1
+        for qj in qint
+            inner = 0
+            i = 1
+            for vi in v
+                inner += Wprime[i,j] * vi
+                i += 1
+            end
+            inner += qj
+            inner = inner % lastDiagonal
+
+            if inner == 0 && C.o[j]
+                inner = lastDiagonal
+            end
+            append!(innerRes, inner)
+            j += 1
+        end
+        #println("inneerres: ", innerRes)
+
+        outerRes = []
+        for l in 1:ambientDimension
+            outer = 0
+            j = 1
+            for innerResi in innerRes
+                outer += C.V[l,j] * innerResi
+                j += 1
+            end
+            append!(outerRes, outer) # outerRes is an integral vector
+        end
+        #println("outerRes: ", outerRes)
+        push!(L, tuple(collect( ((ai + bi) / lastDiagonal) for (ai,bi) in collect(zip(outerRes, qsummand)) )))
+    end
+    return L
 end
+
 
 function solve(A::Matrix{Int64}, b::Vector{Int64})
     C = macmahon(A, b)
     ListOfSymbolicCones = eliminateCoordinates(C, size(C.V, 1) - size(C.V, 2))
     ListOfFundPP = []
+
     for cone in ListOfSymbolicCones
-        push!(ListOfFundPP, enumerateFundamentalParallelePiped(cone))
+        PrintSymbolicCone(cone)
+        append!(ListOfFundPP, enumerateFundamentalParallelePiped(cone))
     end
 
+    #transformIntegral([1 -1])
+    for f in ListOfFundPP
+        for p in f
+            println("p: ", collect(p))
+        end
+    end
+    #println("FPP: ", ListOfFundPP)
     # list of rational_function
 
 end
@@ -206,8 +261,15 @@ end
 
 # println("eliminate cordinates result:\n", eliminateCoordinates(C, size(C.V, 1) - size(C.V, 2)))
 # println("EFP: ", enumerateFundamentalParallelePiped(C))
-println("solve: ", solve([1 -1], [0]))
+#println("solve: ", solve([1 -1], [0]))
+println("solve: ", solve([1 -1; -2 1], [0, 0]))
+V = [1 1; 0 3]
+q = [0.0, 0.0]
+o = [false, false]
+s = SymbolicCone(V, q, o, 1)
+#PrintSymbolicCone(s)
+#res = enumerateFundamentalParallelePiped(s)
+#println("resfpp: ", res)
 #println("prim_v: ", prim_v([-2, 2, 4]))
 #println("prim: ", prim([-2 2 4; 0 3 6]))
-
 end
